43b0b510cf1d9a25c7d479d43a0d28267660b280
brandas <tiagofpbrandao@gmail.com>
2021-02-26 18:35:58 +0100

    animations implemented

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 const header = document.getElementsByTagName("header")[0]
-const nav = document.getElementsByClassName('top-nav-links')[0]
+const nav = document.getElementsByClassName("top-nav-links")[0]
 const burger = document.getElementsByClassName("top-nav-burger")[0]
 const closeBtn = document.getElementsByClassName("close-button")[0]
 
-function openNav() {
-    header.classList.add("show-menu")
+function toggleNav() {
+    if (window.innerWidth <= 600) {
+        header.classList.toggle("show-menu")
+    }
 }
 
-function closeNav() {
-    header.classList.remove("show-menu")
-}
+burger.addEventListener("click", function () {
+  header.classList.toggle("show-menu")
+})
 
-burger.addEventListener("click", openNav)
-closeBtn.addEventListener("click", closeNav)
-nav.addEventListener("click", closeNav)
+nav.addEventListener("click", function () {
+  if (window.innerWidth <= 600) {
+      header.classList.toggle("show-menu")
+  }
+})
 
 window.addEventListener("resize", function () {
-    if (this.innerWidth > 600) {
-      closeNav()
+    if (this.innerWidth > 600 && header.classList.contains("show-menu")) {
+        header.classList.toggle("show-menu")
     }
 })
